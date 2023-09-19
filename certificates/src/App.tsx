877743ec0f7697ab80301fbfcaa9e4fd88378b3d
Olga Stepanova <lechateliermail@gmail.com>
2023-09-19 16:27:36 +0300

    fix modal position and assets structure

--- certificates/src/App.tsx
+++ certificates/src/App.tsx
@@ -1,14 +1,14 @@
-import './App.css'
-import ChemDiploma1 from '../src/assets/1.jpeg';
-import ChemDiploma2 from '../src/assets/2.jpeg';
-import ChemDiploma3 from '../src/assets/3.jpeg';
-import WebDiploma1 from '../src/assets/4.jpeg';
-import WebDiploma2 from '../src/assets/5.jpeg';
-import RSFrontend from '../src/assets/frontend.jpeg';
-import RSReact from '../src/assets/react.jpeg';
+import ChemDiploma1 from '../src/assets/img/1.jpeg';
+import ChemDiploma2 from '../src/assets/img/2.jpeg';
+import ChemDiploma3 from '../src/assets/img/3.jpeg';
+import WebDiploma1 from '../src/assets/img/4.jpeg';
+import WebDiploma2 from '../src/assets/img/5.jpeg';
+import RSFrontend from '../src/assets/img/frontend.jpeg';
+import RSReact from '../src/assets/img/react.jpeg';
 import { Section } from './components/section/Section';
 import { useState } from 'react';
 import { Modal } from './components/Modal/Modal';
+import './App.css'
 
 function App() {
   const [isOpenModal, setOpenModal] = useState(false);
@@ -16,12 +16,18 @@ function App() {
 
   const openModal = (img: string) => {
     setOpenModal(true);
+    document.body.style.overflow = "hidden";
     setCurImage(img);
+  }
+
+  const closeModal = () => {
+    setOpenModal(false);
+    document.body.style.overflow = "auto";
   }
 
   return (
     <main>
-      {isOpenModal && <Modal image={curImage} isOpen={isOpenModal}/>}
+      {isOpenModal && <Modal image={curImage} isOpen={isOpenModal} closeModal={closeModal}/>}
       <Section title="Bachelor's Degree (Chemical Technology)" images={[ChemDiploma1, ChemDiploma2, ChemDiploma3]} openImage={openModal}/>
       <Section title="Diploma of professional retraining" images={[WebDiploma1, WebDiploma2]} openImage={openModal}/>
       <Section title="Frontend course" images={[RSFrontend]} openImage={openModal}/>
--- certificates/src/components/Modal/Modal.tsx
+++ certificates/src/components/Modal/Modal.tsx
@@ -1,14 +1,36 @@
+import { useEffect, useRef } from 'react';
+import CloseBtn from '../../assets/svg/close-circle.svg';
 import styles from './Modal.module.css';
 
 export interface ModalProps {
   image: string;
   isOpen: boolean;
+  closeModal: () => void;
 }
 
-export function Modal({image, isOpen}: ModalProps) {
+export function Modal({image, isOpen, closeModal}: ModalProps) {
+  const modalRef = useRef<HTMLDivElement | null>(null);
+
+  useEffect(() => {
+    function handleClickOutside(event: MouseEvent) {
+      if (modalRef.current && !modalRef.current.contains(event.target as Node)) {
+        closeModal();
+      }
+    }
+    document.addEventListener('mousedown', handleClickOutside);
+    return () => {
+      document.removeEventListener('mousedown', handleClickOutside);
+    };
+  }, [modalRef, closeModal]);
+  
   return (
-    <div className={styles.modal} style={{opacity: `${isOpen ? 1 : 0}`}}>
-      <img src={image} alt="" className={styles.modalImage}/>
+    <div className={styles.modalContainer}>
+      <div className={styles.modal} style={{opacity: `${isOpen ? 1 : 0}`}}>
+        <img src={CloseBtn} className={styles.closeBtn} onClick={closeModal} />
+        <div className={styles.modalContent} ref={modalRef}>
+          <img src={image} alt="" className={styles.modalImage}/>
+        </div>
+      </div>
     </div>
   )
 }
